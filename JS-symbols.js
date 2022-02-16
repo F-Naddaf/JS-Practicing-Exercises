ecb2d08dd2718e1ffecd77ca770994af8ee2752f
fadi <fady-naddaf@hotmail.com>
2022-02-16 14:54:08 +0100

    JS beginner

---
+++ JS-symbols.js
@@ -0,0 +1,8 @@
+const info = { name: 'Julia', age: 10, gender: 'F' };
+
+// the Object is everything inside the curly braces like:
+// < name: 'Julia', age: 10, gender: 'F' >
+
+// the properties is name: 'Julia'
+// the key is name
+// the value is 'julia' typeOf string.
